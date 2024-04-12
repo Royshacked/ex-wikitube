6f57689e0b1e1dee28ca132bb7fabeef6322c99e
Royshacked <roy.shacked@mail.huji.ac.il>
2024-04-12 16:21:35 +0300

    Getting and basic rendering of video and wiki

--- js/service/wikitube.service.js
+++ js/service/wikitube.service.js
@@ -1,1 +1,37 @@
 'use strict'
+
+const YOUTUBE_KEY = 'AIzaSyAHYdeu4hKJj6S_nfyw1kBbOXX1pt4NUsc'
+
+// const YOUTUBE_URL = `
+// https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_KEY}&q=${value}`
+
+// const WIKI_URL = `
+// https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&srsearch=${value}&format=json`
+
+function getVideo(value) {
+    const videos = loadFromStorage(`${value}`)
+    if (videos) return Promise.resolve(videos)
+
+    const url = `
+    https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_KEY}&q=${value}`
+
+    return axios.get(url)
+        .then(results => {
+            saveToStorage(`${value}`, results.data.items)
+            return results.data.items
+        })
+        .catch(err => {
+            console.log(err)
+            throw err
+        })
+}
+
+function getWiki(value) {
+    const url = `https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&srsearch=${value}&format=json`
+    return axios.get(url)
+        .then(results => results.data.query.search)
+        .catch(err => {
+            console.log(err)
+            throw err
+        })
+}
--- js/wikitube.controller.js
+++ js/wikitube.controller.js
@@ -1,1 +1,47 @@
 'use strict'
+
+function onInit() {
+    searchValue()
+}
+
+function onSearch(ev) {
+    ev.preventDefault()
+
+    searchValue()
+}
+
+function searchValue() {
+    const value = document.querySelector('input').value
+    getVideo(value)
+        .then(renderVideo)
+        .then(renderVideos)
+        .catch(err=> alert(err))
+
+    getWiki(value)
+        .then(renderWiki)
+        .catch(err=> alert(err))
+}
+
+function renderVideo(videos) {
+    const videoId = videos[1].id.videoId
+    const strHtml = `<iframe width="420" height="315" src="https://www.youtube.com/embed/${videoId}"></iframe>`
+
+    document.querySelector('.main-video').innerHTML = strHtml
+
+    return videos
+}
+ 
+function renderVideos(videos) {
+    
+}
+
+function renderWiki(results) {
+    const title = results[0].snippet
+    const album = results[1].snippet
+    
+    const strHtml = `
+        <p>${title}</p>
+        <p>${album}</p>
+    `
+    document.querySelector('.main-video').innerHTML += strHtml
+}

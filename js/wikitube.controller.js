ae90b0fbc80013457acd0de9a9bc3315d9746440
Royshacked <roy.shacked@mail.huji.ac.il>
2024-04-12 18:41:50 +0300

    Implement responsive styling

--- js/wikitube.controller.js
+++ js/wikitube.controller.js
@@ -24,10 +24,11 @@ function searchValue() {
 
 function renderVideo(videos) {
     const videoId = videos[0].id.videoId
-    const strHtml = `<iframe width="600" height="350" src="https://www.youtube.com/embed/${videoId}"></iframe>`
+
+    const strHtml = `<iframe width="400" height="300" src="https://www.youtube.com/embed/${videoId}"></iframe>`
 
     document.querySelector('.main-video').innerHTML = strHtml
-
+    // document.querySelector('input').value = ''
     return videos
 }
 
@@ -35,19 +36,21 @@ function renderVideos(videos) {
     const strHtml = videos.map(video =>
         `
             <div class="videos-list-item">
-                <iframe width="200" height="100" src="https://www.youtube.com/embed/${video.id.videoId}"></iframe>
+                <iframe width="125" height="75" src="https://www.youtube.com/embed/${video.id.videoId}"></iframe>
                 <h2>${video.snippet.title}</h2>
             </div>
     `).join('')
 
     document.querySelector('.videos-list').innerHTML = strHtml
+    document.querySelector('input').value = ''
 }
 
 function renderWiki(results) {
     const strHtml = results.map(result =>
-        `
+        `<div class="text-item"> 
             <h2>${result.title}</h2>
             <p>${result.snippet}</p>
+        </div>
         `
     ).join('')
 

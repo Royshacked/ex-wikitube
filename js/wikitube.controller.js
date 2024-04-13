2502d9927e21e8ee99a8ec2f2378468951a60f0a
Royshacked <roy.shacked@mail.huji.ac.il>
2024-04-13 10:48:55 +0300

    Some last touch

--- js/wikitube.controller.js
+++ js/wikitube.controller.js
@@ -1,16 +1,11 @@
 'use strict'
 
 function onInit() {
-    searchValue()
+    onSearchValue()
 }
 
-function onSearch(ev) {
-    ev.preventDefault()
 
-    searchValue()
-}
-
-function searchValue() {
+function onSearchValue() {
     const value = document.querySelector('input').value
     getVideo(value)
         .then(renderVideo)
@@ -22,40 +17,43 @@ function searchValue() {
         .catch(err => alert(err))
 }
 
-function renderVideo(videos) {
+
+function renderVideo(videos,Vidid) {
     const videoId = videos[0].id.videoId
 
     const strHtml = `<iframe width="400" height="300" src="https://www.youtube.com/embed/${videoId}"></iframe>`
 
     document.querySelector('.main-video').innerHTML = strHtml
-    // document.querySelector('input').value = ''
+    document.querySelector('input').value = ''
     return videos
 }
 
+
 function renderVideos(videos) {
     const strHtml = videos.map(video =>
-        `
-            <div class="videos-list-item">
-                <iframe width="125" height="75" src="https://www.youtube.com/embed/${video.id.videoId}"></iframe>
-                <iframe width="125" height="75" src="https://www.youtube.com/embed/${video.id.videoId}"></iframe>
-                <h2>${video.snippet.title}</h2>
-            </div>
-    `).join('')
-
+        `<div class="videos-list-item" >
+            <iframe width="125" height="75" src="https://www.youtube.com/embed/${video.id.videoId}"></iframe>
+            <h2>${video.snippet.title}</h2>
+        </div>`).join('')
+        
     document.querySelector('.videos-list').innerHTML = strHtml
     document.querySelector('input').value = ''
 }
 
+
 function renderWiki(results) {
     const strHtml = results.map(result =>
         `<div class="text-item"> 
-        `<div class="text-item"> 
             <h2>${result.title}</h2>
             <p>${result.snippet}</p>
-        </div>
-        </div>
-        `
-    ).join('')
+        </div>`).join('')
 
     document.querySelector('.main-text').innerHTML = strHtml
+}
+
+
+function onSearch(ev) {
+    ev.preventDefault()
+
+    onSearchValue()
 }
